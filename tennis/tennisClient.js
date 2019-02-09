bb881758bd162b46be9dbe9ed0db573a9f906c02
Mackson Mattos <mackson.mattos@gmail.com>
2019-02-09 20:49:25 -0200

    tennis

--- tennis/tennisClient.js
+++ tennis/tennisClient.js
@@ -111,7 +111,6 @@ class Tennis {
     if (this.status !== "running") this.setStatus("running");
     this.ping = new Date().getTime() - data.time;
 
-    this.me = data.players[this.ticket.id];
     this.state = data;
     this.drawGame();
   }
