e5f29b51559220ca8280c7061b0d9b0c59b37c4e
Mackson Mattos <mackson.mattos@gmail.com>
2019-03-11 17:21:51 -0300

    gh_version

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
 var app = require("express")();
 var server = require("http").Server(app);
 var io = require("socket.io")(server);
-const tennisGame = require("./tennis");
 const skwarzGame = require("./skwarz");
 const serverPort = process.env.PORT || 8000;
 
 server.listen(serverPort);
 app.serverPort = serverPort;
 
-app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/client/index.html");
-});
-
-tennisGame(app, io);
 skwarzGame(app, io);
